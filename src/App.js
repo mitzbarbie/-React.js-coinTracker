71e6ac2f2b7403aa0c296821b6ec7eaf599fb40f
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-11 15:47:44 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>The Coins Information ({coins.length})</h1>
+      {loading ? "loading!!!" : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
